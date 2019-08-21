0655f86d4d8341a814c8b12844fdfd8fe45994c5
Austin Carnahan <ac6404@gmail.com>
2019-08-21 17:44:25 -0500

    switch to intent decorator style

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 from adapt.intent import IntentBuilder
 
 
@@ -6,11 +6,11 @@ class TodayInHistory(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    def initialize(self):
-        today_in_history_intent = IntentBuilder("TodayInHistoryIntent").require("TodayInHistoryKeyword").build()
-        self.register_intent(today_in_history_intent, self.handle_today_in_history_intent)
+    #~ def initialize(self):
+        #~ today_in_history_intent = IntentBuilder("TodayInHistoryIntent").require("TodayInHistoryKeyword").build()
+        #~ self.register_intent(today_in_history_intent, self.handle_today_in_history_intent)
         
-    #~ @intent_file_handler('today.in.history.intent')
+    @intent_handler(IntentBuilder('TodayInHistoryIntent').require("TodayInHistoryKeyword"))
     def handle_today_in_history_intent(self, message):
         self.speak_dialog('searching')
 
